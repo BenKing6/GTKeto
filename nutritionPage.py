a7749fb301873b3c0e375833eadf631693e83b34
Ben King <benking8@hotmail.co.uk>
2021-09-25 13:25:20 +0100

    cleaned up UI layout, refactored nutrition bars

--- nutritionPage.py
+++ nutritionPage.py
@@ -7,23 +7,25 @@ class nutritionPage(Gtk.VBox):
     def __init__(self):
         super().__init__()
         
+        self.set_valign(Gtk.Align.START)
+
         # Nutrition Limit Counters
         self.carbs_bar = Gtk.ProgressBar()
         self.calories_bar = Gtk.ProgressBar()
         self.protein_bar = Gtk.ProgressBar()
         self.fat_bar = Gtk.ProgressBar()
-
-        self.carbs_bar.set_show_text(True)
-        self.calories_bar.set_show_text(True)
-        self.protein_bar.set_show_text(True)
-        self.fat_bar.set_show_text(True)
+        self.nutrition_bars = [self.carbs_bar, self.calories_bar, self.protein_bar, self.fat_bar]
 
         self.carbs_bar.set_text("Total Carbs (g)")
         self.calories_bar.set_text("Total Calories (kcal)")
         self.protein_bar.set_text("Total Protein (g)")
         self.fat_bar.set_text("Total Fat (g)")
 
-        self.add(self.carbs_bar)
-        self.add(self.calories_bar)
-        self.add(self.protein_bar)
-        self.add(self.fat_bar)
+        for bar in self.nutrition_bars:
+            bar.set_show_text(True)
+            bar.set_margin_top(5)
+            bar.set_margin_bottom(5)
+            self.add(bar)
+
+
+
