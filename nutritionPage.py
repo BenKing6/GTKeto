b52cb9a6b4a3d51e5608ed2ef00f04e1022a6a3a
Ben King <benking8@hotmail.co.uk>
2021-09-25 14:54:43 +0100

    added proper LibHandy imports

--- nutritionPage.py
+++ nutritionPage.py
@@ -1,12 +1,17 @@
 import gi
+from nutritionPage import nutritionPage
 
+# GTK 3.0 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+# Libhandy
+gi.require_version('Handy', '1') 
+from gi.repository import Handy
 
 class nutritionPage(Gtk.VBox):
     def __init__(self):
         super().__init__()
-        
+
         # Nutrition Limit Counters
         self.carbs_bar = Gtk.ProgressBar()
         self.calories_bar = Gtk.ProgressBar()
